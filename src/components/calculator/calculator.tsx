5df7ff786b486e6690f9c2d30afa2ae678fddeb5
Mohammed Askar <mdaskar40@gmail.com>
2023-11-15 21:41:41 +0530

    age calculation added

--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -2,44 +2,60 @@ import "./calculator.scss";
 import { FaWandMagicSparkles } from "react-icons/fa6";
 import imageC from "../../assets/imahe.png";
 import { useForm, SubmitHandler } from "react-hook-form";
-import {  useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
 
 interface datas {
   date: string | any;
 }
 
+interface finalValues {
+  days: number;
+  months: number;
+  years: number;
+}
+
 const calculator = () => {
   const [value, setValue] = useState<datas[]>([]);
-
+  const [age, setAge] = useState<finalValues | null>(null);
 
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm<datas>();
 
   const submitE: SubmitHandler<datas> = (data) => {
     setValue([data]);
-   
-    
-    reset();
   };
-  
-  useEffect(()=>{
-    const dateObject = new Date(value[0]?.date);
 
-    // Extract the components
+  useEffect(() => {
+    const dateObject = new Date(value[0]?.date);
+    const today = new Date();
+
     const year = dateObject.getFullYear();
     const month = dateObject.getMonth() + 1; // Month is zero-indexed, so add 1
     const day = dateObject.getDate();
 
-    console.log(`Year: ${year}, Month: ${month}, Day: ${day}`);
-  },[value])
-  
+    let todayDay = today.getDate();
+    let todayMonth = today.getMonth() + 1;
+    let todayYear = today.getFullYear();
 
+    let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+    if (day > todayDay) {
+      todayDay = todayDay + months[todayMonth - 1];
+    }
+    if (month > todayMonth) {
+      todayMonth = todayMonth + 12;
+      todayYear = todayYear - 1;
+    }
+
+    let resultDay = todayDay - day;
+    let resultMonth = todayMonth - month;
+    let resultYear = todayYear - year;
+
+    setAge({ days: resultDay, months: resultMonth, years: resultYear });
+  }, [value]);
 
   return (
     <>
@@ -73,18 +89,45 @@ const calculator = () => {
                 </div>
               </form>
               <div className="resultCont">
-                <div className="result">
-                  <p>-</p>
-                  <span>Years</span>
-                </div>
-                <div className="result">
-                  <p>-</p>
-                  <span>Months</span>
-                </div>
-                <div className="result">
-                  <p>-</p>
-                  <span>Days</span>
-                </div>
+                {age !== null ? (
+                  <>
+                    <div className="result">
+                      <p>
+                        {age?.years !== undefined ? age.years.toString() : "-"}
+                      </p>
+                      <span>Years</span>
+                    </div>
+                    <div className="result">
+                      <p>
+                        {age?.months !== undefined
+                          ? age.months.toString()
+                          : "-"}
+                      </p>
+                      <span>Months</span>
+                    </div>
+                    <div className="result">
+                      <p>
+                        {age?.days !== undefined ? age.days.toString() : "-"}
+                      </p>
+                      <span>Days</span>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="result">
+                      <p>-</p>
+                      <span>Years</span>
+                    </div>
+                    <div className="result">
+                      <p>-</p>
+                      <span>Months</span>
+                    </div>
+                    <div className="result">
+                      <p>-</p>
+                      <span>Days</span>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           </div>
